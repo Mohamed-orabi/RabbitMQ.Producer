6a5bd071c3fe6d489ea46f10fcab0296ea605162
Mohamed orabi <mohamed.orabi97@outlook.com>
2021-10-02 22:43:56 +0200

    Add project files.

---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("Demo", true, false, false, null);
+            var message = new { Name = "Mohamed", message = "Hello" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("", "Demo", null, body);
+
+        }
+    }
+}
